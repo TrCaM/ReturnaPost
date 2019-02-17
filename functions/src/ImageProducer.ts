c38834d4aade5731ffd191a96562505f1b8cda48
Minh Tri Cao <minhtricao@ibm.com>
2019-02-16 19:14:56 -0500

    TODO

---
+++ functions/src/ImageProducer.ts
@@ -0,0 +1,15 @@
+/**
+ * TRI
+ */
+export class ImageProcessor {
+    // Convert image to some uploadable file
+    convertImage(base64String: String)  {
+
+    }
+
+    // Upload the image to gcloud 
+    uploadImage() {
+
+    }
+
+}
