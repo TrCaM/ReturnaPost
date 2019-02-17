de8c07240dec08ac83774d060e44b382eb883855
Zingerlime <lemon.zinger.lime@gmail.com>
2019-02-16 23:34:20 -0500

    update

--- Returner/src/home/home.js
+++ Returner/src/home/home.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 import {
   Platform,
   StyleSheet,
-  Text,
   ScrollView,
   View
 } from 'react-native';
+import {Card, Text} from 'react-native-elements'
 
 class Home extends Component {
 
@@ -17,10 +17,65 @@ class Home extends Component {
 
   render() {
     return (
-      <View style={{flex: 1}}>
+      <View style={styles.container}>
+        <View style={styles.cardContainer}>
+          <Card
+            containerStyle={styles.cardWidth}
+            style={styles.innerCard}
+            image={{uri:'https://static.velkybrands.com/i/items/5418/medium/3345051900.jpg'}}
+          >
+                <View style={styles.flexDirection}>
+                  <View style={styles.item}>
+                    <Text>
+                      Last shipment received
+                    </Text>
+                  </View>
+                  <View style={styles.item}>
+                    <Text>
+                      Date of last shipment received
+                    </Text>
+                  </View>
+                  <View style={styles.item}>
+                    <Text>
+                      Total Shipments Received
+                    </Text>
+                  </View>
+                </View>
+            </Card>
+          <Card 
+            containerStyle={styles.cardWidth}
+            image={{uri:'https://static.velkybrands.com/i/items/5418/medium/3345051900.jpg'}}>
+            <Text style={{marginBottom: 10}}>
+              Last return sent
+            </Text>
+          </Card>
+        </View>
       </View>
     )
   }
 }
-
+const styles = StyleSheet.create({
+  container: {
+    flex:1
+  },
+  cardContainer: {
+    flexDirection: 'row',
+    flex:1
+  },
+  cardWidth: {
+    flexDirection: 'column',
+    flex:1
+  },
+  item: {
+    flex:1
+  },
+  flexDirection: {
+      flexDirection: 'column',
+      flex:1
+  },
+  innerCard: {
+    flexDirection: 'column',
+    flex:1
+  },
+});
 export default Home;
